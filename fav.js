11c5d584b9469ac1fe6a151d7fdf1fd7f9e78076
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-03-24 13:23:12 -0700

    EMBEDED VIDEO AND VIDEO TITLE USING THE API CALL

--- fav.js
+++ fav.js
@@ -1,19 +1,24 @@
-/*function displaySongs(response){
+function displaySongs(response){
     console.log(response.data)
-
+    
+    let musicTitleElement=document.querySelector("#music-title");
+    let musicTitle=response.data.items[1].snippet.title
+    musicTitleElement.innerHTML=musicTitle;
+    let videoElement=document.querySelector("#music-videos")
+    let videoID=response.data.items[1].snippet.resourceId.videoId
+     videoElement.innerHTML =
+ `<iframe width="560" height="315" 
+    src= "https://www.youtube.com/embed/${videoID}"
+    frameborder="0" 
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+    allowfullscreen>
+  </iframe>`;
 }
 
 function displayPlayList(){
-    console.log("Making API request...");
 let apiKey = "AIzaSyD3LHry4BWoKZqBVT_TrjYdDF-iQbOAscI";
 let apiUrl=`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLJakPDSZzpUncHrwcii5Pj8AwUfvSDktR&key=${apiKey}`
 axios.get(apiUrl).then(displaySongs)
-}*/
-
-
-function PlayList(){
-    alert("hell")
-
 }
 let myButton=document.querySelector("#buttonPlayList");
-myButton.addEventListener("click",PlayList);
+myButton.addEventListener("click",displayPlayList);
