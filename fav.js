8a384a38ceb4b1f296055e1352a60ed5ed2ce9e0
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-03-25 13:08:06 -0700

    updated css

--- fav.js
+++ fav.js
@@ -3,7 +3,7 @@ function displaySongs(response){
     let playList=document.querySelector("#playLists")
     playList.innerHTML="";
     let items=response.data.items;
-    for(let i=0; i<4; i++){
+    for(let i=0; i<5; i++){
     let musicTitle=items[i].snippet.title
     let videoID=items[i].snippet.resourceId.videoId
     let videoUrl=`https://www.youtube.com/watch?v=${videoID}`;
@@ -13,7 +13,7 @@ function displaySongs(response){
         <div class="music-title">${musicTitle}
         </div>
         <div class="musicvideo-link" id="musicvideo-link">
-          <a href="${videoUrl}"><img src="${thumbNail}" width=100px/></a>
+          <a href="${videoUrl}"><img src="${thumbNail}" width=100px/>"clickable"</a>
         </div>
       </div>`;
   }
